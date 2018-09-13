c20b1c2fdd3e6a4b3e98b38a8bd403d69f8b3521
Hassan Khan <hassan.khan@endemolshine.com>
2018-09-13 22:37:32 +0100

    refactor: use subclassed errors instead of console.error()

--- src/Steps/AllowStep.ts
+++ src/Steps/AllowStep.ts
@@ -2,25 +2,25 @@ import { isEqual, each, includes } from 'lodash';
 
 import Step from './Step';
 import { Roles, Permissions, Grants } from '../types';
+import AccessControlError from '../Errors/AccessControlError';
 
 export default class AllowStep extends Step {
   toExtend(anotherRole: string):void {
     if (!this.parent.hasRoles(this)) {
-      console.error(`AccessControl Error: AccessControl setup incorrectly. Please set grants before using it`);
-      return;
+      throw new AccessControlError(`AccessControl setup incorrectly. Please set grants before using it`);
     }
 
     const roles: Roles = this.parent.getRoles(this);
 
     if (!roles[this.query.role!]) {
-      console.log(`AccessControl Warning: ${this.query.role} does not exist and will be created`);
+      // throw new AccessControlError(`AccessControl Error: AccessControl setup incorrectly. Please set grants before using it`);
+      // console.log(`AccessControl Warning: ${this.query.role} does not exist and will be created`);
       // return;
       roles[this.query.role!] = {};
     }
 
     if (!roles[anotherRole]) {
-      console.error(`AccessControl Error: Cannot extend ${anotherRole} role because it could not be found in roles`);
-      return;
+      throw new AccessControlError(`Cannot extend ${anotherRole} role because it could not be found in roles`);
     }
 
     const primaryRolePermissions:   Permissions = roles[this.query.role!];
--- src/Steps/PermissionCheckStep.ts
+++ src/Steps/PermissionCheckStep.ts
@@ -4,7 +4,7 @@ import Step from './Step';
 export default class PermissionCheckStep extends Step {
   hasPermission():boolean {
     if (!this.parent.hasRoles(this)) {
-      console.error(`AccessControl Error: AccessControl setup incorrectly. Please set grants before using it`);
+      // console.error(`AccessControl Error: AccessControl setup incorrectly. Please set grants before using it`);
       return false;
     }
 
@@ -13,12 +13,12 @@ export default class PermissionCheckStep extends Step {
     const query = this.query as GrantQuery;
 
     if (!roles[query.role]) {
-      console.error(`AccessControl Error: ${query.role} role could not be found in grants`);
+      // console.error(`AccessControl Error: ${query.role} role could not be found in grants`);
       return false;
     }
 
     if (!roles[query.role][query.permission]) {
-      console.error(`AccessControl Error: ${query.permission} permission could not be found for ${query.role} role`);
+      // console.error(`AccessControl Error: ${query.permission} permission could not be found for ${query.role} role`);
       return false;
     }
 
