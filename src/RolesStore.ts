7de338b0c0a3d5151e1ef39576cef9c36592941c
Hassan Khan <hassan.khan@endemolshine.com>
2018-11-16 09:25:39 +0000

    refactor(acl): move roles handling from accesscontrol
    Roles now are managed inside their own class `RolesStore` and created in `AccessControl` and passed down
    to steps instead of the whole `AccessControl` class.
    
    Fixes #14

---
+++ src/RolesStore.ts
@@ -0,0 +1,51 @@
+import cloneDeep from 'lodash.clonedeep';
+import isEmpty from 'lodash.isempty';
+import omit from 'lodash.omit';
+
+// Errors
+import PermissionNotFoundError from './Errors/PermissionNotFoundError';
+
+// Types
+import { Permissions, Roles } from './types';
+
+export default class RolesStore {
+  private roles: Roles = {};
+
+  constructor(roles?: Roles) {
+    if (roles) {
+      this.setRoles(roles);
+    }
+  }
+
+  setRoles(roles: Roles): void {
+    this.roles = roles;
+  }
+
+  getRoles(): Roles {
+    return cloneDeep(this.roles);
+  }
+
+  getRolesList(): string[] {
+    return Object.keys(this.roles);
+  }
+
+  getPermissions(role: string): Permissions | null {
+    if (isEmpty(this.roles[role])) {
+      throw new PermissionNotFoundError(role);
+    }
+
+    return this.roles[role];
+  }
+
+  modifyRoles(roles: Roles): void {
+    this.roles = roles;
+  }
+
+  hasRoles(): boolean {
+    return !isEmpty(this.roles);
+  }
+
+  removeRoles(roles: string[]) {
+    this.modifyRoles(omit(this.getRoles(), roles));
+  }
+}
